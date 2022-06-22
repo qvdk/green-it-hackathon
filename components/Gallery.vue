f1a21735c18a84246a251c41380585135254dbad
Quentin Vandekerckhove <quentin.vandekerckhove@adeo.com>
2022-06-23 01:25:04 +0200

    feat: introduce an advice galery

--- components/Gallery.vue
+++ components/Gallery.vue
@@ -1,5 +1,43 @@
 <template>
   <v-container>
-    <h2>Gallery - TODO</h2>
+    <v-card flat>
+      <h2>Des solutions durables comme responsable ...</h2>
+      <v-img
+        src="advice1.webp"
+      >
+      </v-img>
+      <v-card-text>
+        Mon Petit Potager a fait le choix de s'investir durablement pour l'environnement.
+      </v-card-text>
+      <v-card-actions>
+        <v-btn to="/advices" nuxt>
+          En savoir plus
+        </v-btn>
+      </v-card-actions>
+    </v-card>
   </v-container>
 </template>
+
+<script>
+  export default {
+    name: 'Galery',
+    data () {
+      return {
+        items: [
+          {
+            src: 'advice1.webp',
+          },
+          {
+            src: 'advice2.webp',
+          },
+          {
+            src: 'advice3.webp',
+          },
+          {
+            src: 'advice4.webp',
+          },
+        ],
+      }
+    },
+  }
+</script>
